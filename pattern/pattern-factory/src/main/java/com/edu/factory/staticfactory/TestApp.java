33ec4b6920386af03d34252ef5989a717bdff2f7
愛么么愛啪啪 <1967646684@qq.com>
2018-07-13 10:32:55 +0800

    test

---
+++ pattern/pattern-factory/src/main/java/com/edu/factory/staticfactory/TestApp.java
@@ -0,0 +1,10 @@
+package com.edu.factory.staticfactory;
+
+/**
+ * Created by Crown on 2018/7/13.
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
