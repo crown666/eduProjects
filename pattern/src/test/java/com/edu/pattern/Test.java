40c895256ca2e6ccfcb1e27a3c09c26c09f7c067
愛么么愛啪啪 <1967646684@qq.com>
2018-07-13 10:27:33 +0800

    test

---
+++ pattern/src/test/java/com/edu/pattern/Test.java
@@ -0,0 +1,10 @@
+package com.edu.pattern;
+
+/**
+ * Created by Crown on 2018/7/13.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
