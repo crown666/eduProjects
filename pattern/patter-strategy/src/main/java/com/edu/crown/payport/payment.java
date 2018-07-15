80129f17f4ab4990a1d45c60532117b53fb84c67
愛么么愛啪啪 <1967646684@qq.com>
2018-07-15 12:03:51 +0800

    test

--- pattern/patter-strategy/src/main/java/com/edu/crown/payport/payment.java
+++ pattern/patter-strategy/src/main/java/com/edu/crown/payport/payment.java
@@ -5,7 +5,14 @@ import com.edu.crown.pay.PayState;
 /**
  * Created by Crown on 2018/7/15.
  * =============
+ * ====
  */
 public interface Payment {
+    /**
+     *
+     * @param uid
+     * @param amount
+     * @return
+     */
     PayState pay(String uid,double amount);
 }
